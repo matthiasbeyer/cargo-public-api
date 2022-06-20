bb7b1279ba2bdcd13415774f1fe20a2ec30161e5
Martin Nordholts <enselic@gmail.com>
2022-06-20 18:19:05 +0200

    wip

--- test-apis/comprehensive_api/src/attributes.rs
+++ test-apis/comprehensive_api/src/attributes.rs
@@ -1,17 +1,26 @@
-#[doc(hidden)]
-pub fn doc_hidden() {}
+#[no_mangle]
+#[link_section = ".custom"]
+pub static NO_MANGLE_WITH_CUSTOM_LINK_SECTION: usize = 42;
 
 #[non_exhaustive]
 pub enum NonExhaustive {
     MoreToCome,
 }
 
-#[must_use]
-pub fn must_use() -> usize {
-    0
-}
-
 #[repr(C)]
 pub struct C {
     pub b: bool,
+}
+
+#[doc(hidden)]
+pub fn doc_hidden() {}
+
+#[export_name = "something_arbitrary"]
+pub fn export_name() {}
+
+// #[must_use] is not shown by cargo doc, so we should not display it either if
+// it is present
+#[must_use]
+pub fn must_use() -> usize {
+    0
 }
